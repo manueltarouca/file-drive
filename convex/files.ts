594d86eb3f1df5dab6bfaecaa92b8186c9588e3f
mt <manuel+git@matm.xyz>
2024-03-03 19:35:19 +0000

    auth with org support

--- convex/files.ts
+++ convex/files.ts
@@ -1,28 +1,49 @@
-import { mutation, query } from './_generated/server';
+import { MutationCtx, QueryCtx, mutation, query } from './_generated/server';
 import { ConvexError, v } from 'convex/values';
+import { getUser } from './users';
+
+async function hasAccessToOrg(ctx: QueryCtx | MutationCtx, tokenIdentifier: string, orgId: string) {
+  const user = await getUser(ctx, tokenIdentifier);
+  return user.orgIds.includes(orgId) || user.tokenIdentifier.includes(orgId);
+}
 
 export const createFile = mutation({
   args: {
     name: v.string(),
+    orgId: v.string(),
   },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
       throw new ConvexError('You must be signed in to create a file');
     }
+    const hasAccess = await hasAccessToOrg(ctx, identity.tokenIdentifier, args.orgId);
+    console.log(identity.tokenIdentifier, args.orgId);
+    console.log('hasAccess', hasAccess);
+    if (!hasAccess) {
+      throw new ConvexError('You do not have access to this organization');
+    }
     await ctx.db.insert('files', {
       name: args.name,
+      orgId: args.orgId,
     });
   },
 });
 
 export const getFiles = query({
-  args: {},
+  args: { orgId: v.string() },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
       return [];
     }
-    return ctx.db.query('files').collect();
+    const hasAccess = await hasAccessToOrg(ctx, identity.tokenIdentifier, args.orgId);
+    if (!hasAccess) {
+      return [];
+    }
+    return ctx.db
+      .query('files')
+      .withIndex('by_orgId', (q) => q.eq('orgId', args.orgId))
+      .collect();
   },
 });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import ConvexClientProvider from './ConvexClientProvider';
 import { ClerkProvider } from '@clerk/nextjs';
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import { Header } from './header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider>
+          <Header />
+          {children}
+        </ConvexClientProvider>
       </body>
     </html>
   );
